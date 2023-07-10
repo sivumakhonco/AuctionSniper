d630832331d6d9a5d037d4458e259199a5840ce8
Sivu Makhonco <sivu.makhonco@gmail.com>
2023-07-10 18:57:19 +0200

    added first end-to-end test

--- AuctionSniper/AuctionSniper.Application/Interfaces/IAuctionServer.cs
+++ AuctionSniper/AuctionSniper.Application/Interfaces/IAuctionServer.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
-namespace AuctionSniper.Application.Interfaces
+namespace AuctionSniper.Application.Interfaces;
+
+public interface IAuctionServer
 {
-	public interface IAuctionServer
-	{
-		public ValueTask StartSellingItem();
-		public ValueTask HasReceivedJoinRequestFromSniper();
-		public ValueTask AnnounceClosed();
-	}
+	public ValueTask StartSellingItem();
+	public ValueTask HasReceivedJoinRequestFromSniper();
+	public ValueTask AnnounceClosed();
 }
